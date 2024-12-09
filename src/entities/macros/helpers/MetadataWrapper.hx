b13a5da18d752df3ca62141dcd84ea87da0dfa7f
Ian Harrigan <ianharrigan@hotmail.com>
2024-12-09 18:27:08 +0100

    move import

--- src/entities/macros/helpers/MetadataWrapper.hx
+++ src/entities/macros/helpers/MetadataWrapper.hx
@@ -1,8 +1,8 @@
 package entities.macros.helpers;
 
-import haxe.macro.Context;
 #if macro
 
+import haxe.macro.Context;
 import haxe.macro.ExprTools;
 import haxe.macro.Expr;
 import haxe.macro.Type;
