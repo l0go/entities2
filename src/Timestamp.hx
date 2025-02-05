3e977a4dd3470e9d1a2ad57754c36b42d5c04ba2
shallowmallow <maud.davout@gmail.com>
2025-02-05 11:53:41 +0100

    adding comparisons for Timestamps

--- src/Timestamp.hx
+++ src/Timestamp.hx
@@ -4,4 +4,7 @@ abstract Timestamp(Float) to Float from Float {
 	public static function now():Timestamp {
 		return Date.now().getTime();
 	}
+
+	@:op(A > B) static function gt(a:Timestamp, b:Timestamp):Bool;
+	@:op(B < A) static function lt(a:Timestamp, b:Timestamp):Bool;
 }
