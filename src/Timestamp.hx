930448b76e89bc252b6e40826a6df8740cdab279
Ian Harrigan <ianharrigan@hotmail.com>
2025-03-14 11:09:44 +0100

    @:op(A - B)

--- src/Timestamp.hx
+++ src/Timestamp.hx
@@ -6,6 +6,7 @@ abstract Timestamp(Float) to Float from Float {
 	}
 
 	@:op(A + B) static function add(a:Timestamp, b:Timestamp):Timestamp;
+	@:op(A - B) static function add(a:Timestamp, b:Timestamp):Timestamp;
 	@:op(A > B) static function gt(a:Timestamp, b:Timestamp):Bool;
 	@:op(B < A) static function lt(a:Timestamp, b:Timestamp):Bool;
 }
